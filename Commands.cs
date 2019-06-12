5bbddf4622cebe145833bd6036c474254657ed29
Kerbinorbiterr <35101977+Kerbinorbiterr@users.noreply.github.com>
2019-06-12 20:04:29 +0100

    Add files via upload
    REQUIRES discord.net application thing

---
+++ Commands.cs
@@ -0,0 +1,189 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+using System.IO;
+
+//discord
+using Discord;
+using Discord.WebSocket;
+using Discord.Commands;
+using Discord.Net;
+using Discord.API;
+using Discord.Rest;
+namespace Katie2
+{
+    [Name("BasicCommands")]
+    public class Commands : ModuleBase
+    {
+        [Command("ping")]
+        public async Task Ping()
+        {
+            await Context.Channel.SendMessageAsync($"<@{Context.User.Id}>");
+        }
+        [Command("Kill")]
+        public async Task Poweroff()
+        {
+            if (Context.User.Id == 331159190288596993)
+            {
+                await Context.Channel.SendMessageAsync("K.A.T.I.E shutting down...");
+                await Program._client.SetStatusAsync(UserStatus.Offline);
+                Environment.Exit(0);
+            } else
+            {
+                await Context.Channel.SendMessageAsync("Sorry, $<@{Context.User.Id}> You may not Use that command");
+            }
+        }
+        [Command("About")]
+        public async Task About()
+        {
+            await Context.Channel.SendMessageAsync("```K.A.T.I.E        v.1.0" +
+            "Developer(s): Eva#9914```");
+        }
+        // [Command("kick")]
+        /*public async Task KickUser(ulong userId, string reason)
+        {
+            var user = Program.Program.client.GetUser(userId) as IGuildUser;
+            if (((Context.User as IGuildUser).GuildPermissions.KickMembers || Context.User.Id == 366298290377195522) && (Program.client.CurrentUser as IGuildUser).GuildPermissions.KickMembers)
+            {
+                await user.KickAsync(reason);
+            }
+            else
+            {
+                await Context.Channel.SendMessageAsync("One of us doesn't have that permission...");
+            }
+        }*/
+        [Command("Time")]
+        public async Task GetTime()
+        {
+            await Context.Channel.SendMessageAsync("The time in the UK (GMT/BST) Is: " + DateTime.Now.ToString("h:mm:ss tt"));
+        }
+        [Command("Date")]
+        public async Task GetDay()
+        {
+            await Context.Channel.SendMessageAsync("Today is: " + DateTime.Now.ToString("D"));
+        }
+        [Command("Wednesday")]
+        public async Task WednesdayMyDudes()
+        {
+            if(DateTime.Now.ToString("D") == "Wed")
+            {
+                await Context.Channel.SendMessageAsync("It is Wednesday my dude :)");
+            } else
+            {
+                await Context.Channel.SendMessageAsync("Unfortunately it is not Wednesday my dude :(");
+            }
+        }
+        //[Command("Help")]
+        //public async Task Help()
+        //{
+            //var helpEmbed = new EmbedBuilder
+            //{
+              //  Color = Color.Gold
+            //};
+        //}
+        [Command("Source")]
+        public async Task GetSourceCode()
+        {
+            await Context.Channel.SendFileAsync(@"C:\Users\Kerbinorbiter\Documents\K.A.T.I.E\Katie2\Source.cs","K.A.T.I.E bot source code:");
+        }
+        [Command("Flip Coin")]
+        public async Task CoinFlip(string HeadsOrTails)
+        {
+            if(HeadsOrTails == "Heads" | HeadsOrTails == "Tails")
+            {
+                Random rnd = new Random();
+                RandomNumberGenerator cryptoRnd = RandomNumberGenerator.Create();
+                int output = rnd.Next(1, 3);
+                if(output < 2.5)
+                {
+                    if(HeadsOrTails == "Heads")
+                    {
+                        await Context.Channel.SendMessageAsync("You Won");
+                        return;
+                    } else
+                    {
+                        await Context.Channel.SendMessageAsync("You Lost");
+                        return;
+                    }
+                } else
+                {
+                    if(HeadsOrTails == "Heads")
+                    {
+                        await Context.Channel.SendMessageAsync("You Lost");
+                        return;
+                    } else
+                    {
+                        await Context.Channel.SendMessageAsync("You Won");
+                        return;
+                    }
+                }
+            }
+            else
+            {
+                await Context.Channel.SendMessageAsync("Unknown Result: " + HeadsOrTails + " Please use *Heads* or *Tails*");
+            }
+        }
+        //KSP Command groupings
+        /*[Command("OrbVelocity")]
+        public async Task OrbitalVeloctyCalc(double Apoapse, double Periapse, double altitude, double planetRadius)
+        {
+            double apaINTERNAL = Apoapse * 1000; //converts from kelometers into meters
+            double pepINTERNAL = Periapse * 1000; //converts from km to m
+            double planetRadiusINTERNAL = planetRadius * 1000;
+            double internalAlt = planetRadiusINTERNAL+(altitude * 1000);
+            double smaINTERNAL = ((apaINTERNAL + planetRadiusINTERNAL) + (pepINTERNAL + planetRadiusINTERNAL)) / 2;
+            double gCONST = Math.Pow(6.67, -11);
+            double v = Math.Pow(gCONST * (2 / internalAlt - 1 / smaINTERNAL), 0.5);
+            double vROUND = Math.Round(v, 1);
+            await Context.Channel.SendMessageAsync("Velocity:" + vROUND.ToString() + " m/s");
+        }*/
+        [Command("Azimuth")]
+        public async Task AzimuthCalc(double i, double lat)
+        {
+            double azm = Math.Round(Math.Tanh(Math.Cos(i) / Math.Cos(lat)),1);
+            await Context.Channel.SendMessageAsync("Azimuth: " + azm.ToString() + " Degrees Heading North");
+        }
+        [Command("Boostback")]
+        public async Task BoostbackCalculation(double startMass, double StageMass, double StageVelocity)
+        {
+            double massRatio = StageMass/startMass;
+            double boostbackEndVel = StageVelocity * massRatio;
+            double manuvureVelocity = StageVelocity + boostbackEndVel;
+            await Context.Channel.SendMessageAsync("Boostback End Velocity: " + Math.Round(boostbackEndVel, 1).ToString() + " m/s");
+            await Context.Channel.SendMessageAsync("Boostback Manuvure DeltaV: " + Math.Round(manuvureVelocity, 1).ToString() + " m/s");
+        }
+        [Command("Root")]
+        public async Task Root(double rootInt, double exponent)
+        {
+            try
+            {
+                double rtRES = Math.Pow(rootInt, 1/exponent); //using a newtonian method
+                await Context.Channel.SendMessageAsync("Result of: ROOT" + exponent.ToString() + "(" + rootInt.ToString() + ") = " + rtRES.ToString());
+            } catch
+            {
+                await Context.Channel.SendMessageAsync("Error: you must enter an *integer* (Whole Number) for this operation");
+            }
+        }
+        [Command("Warn")]
+        public async Task WarnUser(ulong usrID, string warnReason)
+        {
+            /*setup variables such as user id, moderator id and discriminators to allow for the bot to do the following:
+             * 1: display a message which states that user x has been warned by user y for doing z
+             * 2: log that into a specific channel
+             * 3: DM the user being warned that they have been warned in server x by user y for doing z
+             */
+            var warned = Program._client.GetUser(usrID) as IGuildUser;
+            var authority = Context.Message.Author;
+            var embedWarn = new EmbedBuilder
+            {
+                Color = Color.DarkRed
+            };
+            embedWarn.Description = "User: " + warned.Username + "#" + warned.Discriminator + " By: " + authority.Username + "#" + authority.Discriminator + " For: " + warnReason;
+
+        }
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+using Microsoft.Extensions.DependencyInjection;
+//discord
+/*credits:
+ * Support and dev support: Mrcarrot#3305
+ * idea: Katie#6969
+ * Developer: Eva#9914/Kerbinorbiter#9914
+ */
+using Discord;
+using Discord.WebSocket;
+using Discord.Commands;
+using Discord.Net;
+using Discord.API;
+using Discord.Rest;
+namespace Katie2
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+            => new Program().MainAsync().GetAwaiter().GetResult();
+
+        public static DiscordSocketClient _client;
+        private CommandService commands;
+        private IServiceProvider services;
+        public async Task MainAsync()
+        {
+            _client = new DiscordSocketClient();
+            commands = new CommandService();
+            services = new ServiceCollection().BuildServiceProvider();
+            await InstallCommands();
+
+            _client.Log += Log;
+
+            // Remember to keep token private or to read it from an 
+            // external source! In this case, we are reading the token 
+            // from an environment variable. If you do not know how to set-up
+            // environment variables, you may find more information on the 
+            // Internet or by using other methods such as reading from 
+            // a configuration.
+            await _client.LoginAsync(TokenType.Bot, "NTg4MTE1NTQ3OTEzMzIyNTM3.XQAiVQ.qD-nOJ0er3Ja1OWlIuiDMDuywBk");
+            await _client.StartAsync();
+
+            // Block this task until the program is closed.
+            await Task.Delay(-1);
+        }
+        private Task Log(LogMessage msg)
+        {
+            Console.WriteLine(msg.ToString());
+            return Task.CompletedTask;
+        }
+        public async Task InstallCommands()
+        {
+            // Hook the MessageReceived Event into our Command Handler
+            _client.MessageReceived += HandleCommand;
+            // Discover all of the commands in this assembly and load them.
+            await commands.AddModulesAsync(Assembly.GetEntryAssembly(), services);
+        }
+        public async Task HandleCommand(SocketMessage messageParam)
+        {
+            // Don't process the command if it was a System Message
+            var message = messageParam as SocketUserMessage;
+            if (message == null) return;
+            // Create a number to track where the prefix ends and the command begins
+            int argPos = 0;
+            bool messageIsCommand = message.HasCharPrefix('-', ref argPos);
+            if (!messageIsCommand)
+            {
+                argPos = 0;
+                messageIsCommand = message.HasStringPrefix("KB- ", ref argPos);
+            }
+            if (!messageIsCommand) return;
+            // Create a Command Context
+            var context = new CommandContext(_client, message);
+            // Execute the command. (result does not indicate a return value, 
+            // rather an object stating if the command executed successfully)
+            var result = await commands.ExecuteAsync(context, argPos, services);
+            if (!result.IsSuccess)
+                await context.Channel.SendMessageAsync(result.ErrorReason);
+        }
+    }
+}
