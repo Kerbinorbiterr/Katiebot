b41f0fbda80bb100337faf724a5a22ed6ed54b03
Kerbinorbiterr <35101977+Kerbinorbiterr@users.noreply.github.com>
2019-06-12 22:41:36 +0100

    Update Commands.cs

--- Commands.cs
+++ Commands.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -31,7 +31,8 @@ namespace Katie2
                 await Context.Channel.SendMessageAsync("K.A.T.I.E shutting down...");
                 await Program._client.SetStatusAsync(UserStatus.Offline);
                 Environment.Exit(0);
-            } else
+            }
+            else
             {
                 await Context.Channel.SendMessageAsync("Sorry, $<@{Context.User.Id}> You may not Use that command");
             }
@@ -68,10 +69,11 @@ namespace Katie2
         [Command("Wednesday")]
         public async Task WednesdayMyDudes()
         {
-            if(DateTime.Now.ToString("D") == "Wed")
+            if (DateTime.Now.ToString("D") == "Wed")
             {
                 await Context.Channel.SendMessageAsync("It is Wednesday my dude :)");
-            } else
+            }
+            else
             {
                 await Context.Channel.SendMessageAsync("Unfortunately it is not Wednesday my dude :(");
             }
@@ -79,42 +81,45 @@ namespace Katie2
         //[Command("Help")]
         //public async Task Help()
         //{
-            //var helpEmbed = new EmbedBuilder
-            //{
-              //  Color = Color.Gold
-            //};
+        //var helpEmbed = new EmbedBuilder
+        //{
+        //  Color = Color.Gold
+        //};
         //}
         [Command("Source")]
         public async Task GetSourceCode()
         {
-            await Context.Channel.SendFileAsync(@"C:\Users\Kerbinorbiter\Documents\K.A.T.I.E\Katie2\Source.cs","K.A.T.I.E bot source code:");
+            await Context.Channel.SendFileAsync(@"C:\Users\Kerbinorbiter\Documents\K.A.T.I.E\Katie2\Source.cs", "K.A.T.I.E bot source code:");
         }
         [Command("Flip Coin")]
         public async Task CoinFlip(string HeadsOrTails)
         {
-            if(HeadsOrTails == "Heads" | HeadsOrTails == "Tails")
+            if (HeadsOrTails == "Heads" | HeadsOrTails == "Tails")
             {
                 Random rnd = new Random();
                 RandomNumberGenerator cryptoRnd = RandomNumberGenerator.Create();
                 int output = rnd.Next(1, 3);
-                if(output < 2.5)
+                if (output < 2.5)
                 {
-                    if(HeadsOrTails == "Heads")
+                    if (HeadsOrTails == "Heads")
                     {
                         await Context.Channel.SendMessageAsync("You Won");
                         return;
-                    } else
+                    }
+                    else
                     {
                         await Context.Channel.SendMessageAsync("You Lost");
                         return;
                     }
-                } else
+                }
+                else
                 {
-                    if(HeadsOrTails == "Heads")
+                    if (HeadsOrTails == "Heads")
                     {
                         await Context.Channel.SendMessageAsync("You Lost");
                         return;
-                    } else
+                    }
+                    else
                     {
                         await Context.Channel.SendMessageAsync("You Won");
                         return;
@@ -143,42 +148,89 @@ namespace Katie2
         [Command("Azimuth")]
         public async Task AzimuthCalc(double i, double lat)
         {
-            double azm = Math.Round(Math.Tanh(Math.Cos(i) / Math.Cos(lat)),1);
+            double azm = Math.Round(Math.Tanh(Math.Cos(i) / Math.Cos(lat)), 1);
             await Context.Channel.SendMessageAsync("Azimuth: " + azm.ToString() + " Degrees Heading North");
         }
         [Command("Boostback")]
         public async Task BoostbackCalculation(double startMass, double StageMass, double StageVelocity)
         {
-            double massRatio = StageMass/startMass;
+            double massRatio = StageMass / startMass;
             double boostbackEndVel = StageVelocity * massRatio;
             double manuvureVelocity = StageVelocity + boostbackEndVel;
             await Context.Channel.SendMessageAsync("Boostback End Velocity: " + Math.Round(boostbackEndVel, 1).ToString() + " m/s");
             await Context.Channel.SendMessageAsync("Boostback Manuvure DeltaV: " + Math.Round(manuvureVelocity, 1).ToString() + " m/s");
         }
         [Command("Root")]
-        public async Task Root(double rootInt, double exponent)
+        public async Task Root(int rootInt, int exponent) //I changed these to ints for you- Carrot
         {
             try
             {
-                double rtRES = Math.Pow(rootInt, 1/exponent); //using a newtonian method
+                double rtRES = Math.Pow(rootInt, 1 / exponent); //using a newtonian method
                 await Context.Channel.SendMessageAsync("Result of: ROOT" + exponent.ToString() + "(" + rootInt.ToString() + ") = " + rtRES.ToString());
-            } catch
+            }
+            catch
             {
                 await Context.Channel.SendMessageAsync("Error: you must enter an *integer* (Whole Number) for this operation");
             }
         }
-        [Command("Warn")]
-        public async Task WarnUser(string user, string warnReason)
+        [Command("admin warn")]
+        [RequireUserPermission(GuildPermission.KickMembers)]
+        public async Task Warn(string user, [Remainder]string warnReason)
         {
             /*setup variables such as user id, moderator id and discriminators to allow for the bot to do the following:
              * 1: display a message which states that user x has been warned by user y for doing z
              * 2: log that into a specific channel
              * 3: DM the user being warned that they have been warned in server x by user y for doing z
              */
-            var warned = Program._client.GetUser(CommandUtils.GetID(user)) as IGuildUser;
-            var authority = Context.Message.Author;
-            await Context.Channel.SendMessageAsync("Warn: " + warned.Username + "#" + warned.Discriminator.ToString() + " For: " + warnReason);
+            try
+            {
+                var warned = Context.Guild.GetUsersAsync().Result.ToList().FirstOrDefault(x => x.Id == CommandUtils.GetID(user));
+                var authority = Context.Message.Author;
+                await Context.Channel.SendMessageAsync($"Warn: {warned.Username}#{warned.Discriminator} For: {warnReason}");
+            }
+            catch (Exception e)
+            {
+                await Context.Channel.SendMessageAsync(e.ToString());
+            }
 
+        }
+        [Command("Kick")]
+        [RequireUserPermission(GuildPermission.KickMembers)]
+        public async Task Kick(string user, [Remainder]string kickReason)
+        {
+            var kicked = Context.Guild.GetUsersAsync().Result.ToList().FirstOrDefault(x => x.Id == CommandUtils.GetID(user));
+            var kicker = Context.User;
+            await Context.Channel.SendMessageAsync($"Kick: { kicked.Username}#{ kicked.Discriminator} By: **{kicker.Username}#{kicker.Discriminator}** For: {kickReason}");
+            await kicked.KickAsync();
+        }
+        [Command("Bannir")]
+        [RequireUserPermission(GuildPermission.BanMembers)]
+        public async Task Bannir(string user, [Remainder]string bannirReason)
+        {
+            var bannired = Context.Guild.GetUsersAsync().Result.ToList().FirstOrDefault(x => x.Id == CommandUtils.GetID(user));
+            var bannirer = Context.User;
+            if(bannired.Id == 81805731128414208 || bannired.Id == 588470417317036062) //bannir vanir
+                await Context.Channel.SendFileAsync(@"D:\KATIEBOT\BANNIRVANNIR.png", $"Bannir: **{ bannired.Username}#{ bannired.Discriminator}** By: **{bannirer.Username}#{bannirer.Discriminator}** For: {bannirReason}"); 
+            else
+                await Context.Channel.SendMessageAsync($"Bannir: **{ bannired.Username}#{ bannired.Discriminator}** By: **{bannirer.Username}#{bannirer.Discriminator}** For: {bannirReason}");
+            await bannired.BanAsync();
+        }
+        [Command("Ban")]
+        [RequireUserPermission(GuildPermission.BanMembers)]
+        public async Task Ban(string user, [Remainder]string banReason)
+        {
+            var banned = Context.Guild.GetUsersAsync().Result.ToList().FirstOrDefault(x => x.Id == CommandUtils.GetID(user));
+            var banner = Context.User;
+            await Context.Channel.SendMessageAsync($"Ban: **{ banned.Username}#{ banned.Discriminator}** By: **{banner.Username}#{banner.Discriminator}** For: {banReason}");
+            await banned.BanAsync();
+        }
+        [Command("Challenge Suggest")]
+        public async Task Suggestions([Remainder]string challengeSuggestion)
+        {
+            var channel = Program._client.GetChannel(588128243920535582) as SocketTextChannel;
+            var suggestion = challengeSuggestion;
+            var userSuggestion = Context.User;
+            await channel.SendMessageAsync($"Suggestion by: {userSuggestion.Username}#{userSuggestion.Discriminator}, {challengeSuggestion}");
         }
     }
     public class CommandUtils
@@ -201,6 +253,6 @@ namespace Katie2
                 throw new FormatException(); //The string wasn't formatted correctly if the ID was invalid
                 return 0;
             }
-        }   
+        }
     }
 }
