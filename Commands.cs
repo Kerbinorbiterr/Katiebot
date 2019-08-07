28804d6973bc527df609c373a7816037a9656bcc
Kerbinorbiterr <35101977+Kerbinorbiterr@users.noreply.github.com>
2019-08-07 18:04:00 +0100

    Update Commands.cs

--- Commands.cs
+++ Commands.cs
@@ -1,26 +1,23 @@
 using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
-using System.Security.Cryptography;
-using System.IO;
-using System.Threading;
-
-//discord
 using Discord;
-using Discord.WebSocket;
 using Discord.Commands;
-using Discord.Net;
-using Discord.API;
-using Discord.Rest;
+using Discord.WebSocket;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Http;
+using System.IO;
+using System.Diagnostics;
+using Discord.Audio;
+using System.Threading;
+using System.Security.Cryptography;
+using System.Linq;
 namespace Katie2
 {
     [Name("BasicCommands")]
     public class Commands : ModuleBase
     {
-        private static ulong[] trusted = { 331159190288596993, 165111379496271872, 366298290377195522 }; //gonna take it 4 a walk
-        public static List<ulong> TrustedUsers = trusted.ToList(); //People who can do special stuff
         [RequireUserPermission(GuildPermission.ManageMessages)]
         [Command("clear")]
         public async Task Clear(int numberMsgs)
@@ -83,20 +80,6 @@ namespace Katie2
                 await Context.Channel.SendMessageAsync("Unfortunately it is not Wednesday my dude :(");
             }
         }
-        //[Command("Help")]
-        //public async Task Help()
-        //{
-        //var helpEmbed = new EmbedBuilder
-        //{
-        //  Color = Color.Gold
-        //};
-        //}
-        [Command("Source")]
-        public async Task GetSourceCode()
-        {
-            await Context.Channel.SendFileAsync(@"D:\KATIEBOT\Katie2\Commands.cs", "K.A.T.I.E. Command Source Code"); //sourced from itself to ensure editing it doesnt mean having to copy it over
-            await Context.Channel.SendFileAsync(@"D:\KATIEBOT\Program.cs", "K.A.T.I.E. Boot up code"); //sourced from out side to keep yer dirty mits off my token
-        }
         [Command("Flip Coin")]
         public async Task CoinFlip(string HeadsOrTails)
         {
@@ -138,23 +121,22 @@ namespace Katie2
             }
         }
         //KSP Command groupings
-        /*[Command("OrbVelocity")]
-        public async Task OrbitalVeloctyCalc(double Apoapse, double Periapse, double altitude, double planetRadius)
+        [Command("OrbVelocity")]
+        public async Task OrbitalVeloctyCalc(double Apoapse, double Periapse, double altitude)
         {
             double apaINTERNAL = Apoapse * 1000; //converts from kelometers into meters
             double pepINTERNAL = Periapse * 1000; //converts from km to m
-            double planetRadiusINTERNAL = planetRadius * 1000;
-            double internalAlt = planetRadiusINTERNAL+(altitude * 1000);
+            double planetRadiusINTERNAL = 600 * 1000;
+            double internalAlt = planetRadiusINTERNAL + (altitude * 1000);
             double smaINTERNAL = ((apaINTERNAL + planetRadiusINTERNAL) + (pepINTERNAL + planetRadiusINTERNAL)) / 2;
-            double gCONST = Math.Pow(6.67, -11);
-            double v = Math.Pow(gCONST * (2 / internalAlt - 1 / smaINTERNAL), 0.5);
+            double v = Math.Pow(Math.Pow(5.2915158, 22) * (2 / internalAlt - 1 / smaINTERNAL), 0.5);
             double vROUND = Math.Round(v, 1);
             await Context.Channel.SendMessageAsync("Velocity:" + vROUND.ToString() + " m/s");
-        }*/
+        }
         [Command("Azimuth")]
         public async Task AzimuthCalc(double i, double lat)
         {
-            double azm = Math.Round(Math.Tanh(Math.Cos(i) / Math.Cos(lat)), 1);
+            double azm = Math.Asin(Math.Cos(i) / Math.Cos(lat));
             await Context.Channel.SendMessageAsync("Azimuth: " + azm.ToString() + " Degrees Heading North");
         }
         [Command("Boostback")]
@@ -166,8 +148,42 @@ namespace Katie2
             await Context.Channel.SendMessageAsync("Boostback End Velocity: " + Math.Round(boostbackEndVel, 1).ToString() + " m/s");
             await Context.Channel.SendMessageAsync("Boostback Manuvure DeltaV: " + Math.Round(manuvureVelocity, 1).ToString() + " m/s");
         }
+        [Command("dV")] //returns the delta V you have
+        public async Task DeltaV(double isp, double m0, double m1)
+        {
+            double g0 = 9.8067;
+            double dV = (isp * g0) * Math.Log(m1 / m0);
+            await Context.Channel.SendMessageAsync("dV: " + dV.ToString() + " m/s");
+        }
+        [Command("Pythagoras")]
+        public async Task PythagoreamTheorem(double a, double b, string missingValue)
+        {
+            if (missingValue == "Longest") //asks the user if they are wanting the hypotonuese
+            {
+                double h = Math.Sqrt((Math.Pow(a, 2) + Math.Pow(b, 2)));
+                await Context.Channel.SendMessageAsync("Length:" + Math.Round(h, 1));
+                return;
+            }
+            else
+            {
+                double h = Math.Sqrt((Math.Pow(a, 2) - Math.Pow(b, 2)));
+                await Context.Channel.SendMessageAsync("Length:" + Math.Round(h, 1));
+                return;
+            }
+        }
+        [Command("Changelog")] //grabs a changelog
+        public async Task GetChangelog()
+        {
+            var EmbedChangelog = new EmbedBuilder()
+            {
+                Color = Color.DarkOrange
+            };
+            EmbedChangelog.Description = $"{File.ReadAllText(@"D:\KATIEBOT\CHANGELOG.Katie")}";
+            EmbedChangelog.Title = "CHANGELOG";
+            await Context.User.SendMessageAsync("", false, EmbedChangelog.Build());
+        }
         [Command("Root")]
-        public async Task Root(int rootInt, int exponent) //I changed these to ints for you- Carrot
+        public async Task Root(double rootInt, double exponent)
         {
             try
             {
@@ -179,7 +195,7 @@ namespace Katie2
                 await Context.Channel.SendMessageAsync("Error: you must enter an *integer* (Whole Number) for this operation");
             }
         }
-        [Command("admin warn")]
+        [Command("warn")]
         [RequireUserPermission(GuildPermission.KickMembers)]
         public async Task Warn(string user, [Remainder]string warnReason)
         {
@@ -190,9 +206,13 @@ namespace Katie2
              */
             try
             {
+                var WarnInfoEmbed = new EmbedBuilder()
+                {
+                    Color = Color.Red
+                };
                 var warned = Context.Guild.GetUsersAsync().Result.ToList().FirstOrDefault(x => x.Id == CommandUtils.GetID(user));
                 var authority = Context.Message.Author;
-                await Context.Channel.SendMessageAsync($"Warn: {warned.Username}#{warned.Discriminator} For: {warnReason}");
+                await Context.Channel.SendMessageAsync($"Warn: {warned.Username}#{warned.Discriminator} For: {warnReason} By: {authority.Username}#{authority.Discriminator}");
             }
             catch (Exception e)
             {
@@ -204,6 +224,7 @@ namespace Katie2
         [RequireUserPermission(GuildPermission.KickMembers)]
         public async Task Kick(string user, [Remainder]string kickReason)
         {
+            //kicks a user
             var kicked = Context.Guild.GetUsersAsync().Result.ToList().FirstOrDefault(x => x.Id == CommandUtils.GetID(user));
             var kicker = Context.User;
             await Context.Channel.SendMessageAsync($"Kick: { kicked.Username}#{ kicked.Discriminator} By: **{kicker.Username}#{kicker.Discriminator}** For: {kickReason}");
@@ -215,8 +236,8 @@ namespace Katie2
         {
             var bannired = Context.Guild.GetUsersAsync().Result.ToList().FirstOrDefault(x => x.Id == CommandUtils.GetID(user));
             var bannirer = Context.User;
-            if(bannired.Id == 81805731128414208 || bannired.Id == 588470417317036062) //bannir vanir
-                await Context.Channel.SendFileAsync(@"D:\KATIEBOT\BANNIRVANNIR.png", $"Bannir: **{ bannired.Username}#{ bannired.Discriminator}** By: **{bannirer.Username}#{bannirer.Discriminator}** For: {bannirReason}"); 
+            if (bannired.Id == 81805731128414208 || bannired.Id == 588470417317036062) //bannir vanir
+                await Context.Channel.SendFileAsync(@"D:\KATIEBOT\BANNIRVANNIR.png", $"Bannir: **{ bannired.Username}#{ bannired.Discriminator}** By: **{bannirer.Username}#{bannirer.Discriminator}** For: {bannirReason}");
             else
                 await Context.Channel.SendMessageAsync($"Bannir: **{ bannired.Username}#{ bannired.Discriminator}** By: **{bannirer.Username}#{bannirer.Discriminator}** For: {bannirReason}");
             await bannired.BanAsync();
@@ -225,6 +246,7 @@ namespace Katie2
         [RequireUserPermission(GuildPermission.BanMembers)]
         public async Task Ban(string user, [Remainder]string banReason)
         {
+            //bans a user
             var banned = Context.Guild.GetUsersAsync().Result.ToList().FirstOrDefault(x => x.Id == CommandUtils.GetID(user));
             var banner = Context.User;
             await Context.Channel.SendMessageAsync($"Ban: **{ banned.Username}#{ banned.Discriminator}** By: **{banner.Username}#{banner.Discriminator}** For: {banReason}");
@@ -239,66 +261,51 @@ namespace Katie2
             await channel.SendMessageAsync($"Suggestion by: {userSuggestion.Username}#{userSuggestion.Discriminator}, {challengeSuggestion}");
         }
         [Command("Broadcast")]
+        [RequireUserPermission(GuildPermission.Administrator)]
+        //allows anyone to broadcast a message if they have adminstrator perms and if the message destination is in the same server
         public async Task BroadcastMessage(string chnStr, [Remainder]string broadcastMessage)
         {
-            if (!TrustedUsers.Contains(Context.User.Id)) return; //https://tenor.com/view/im-gonna-stop-you-right-there-gif-10559828
             var channel = Program._client.GetChannel(CommandUtils.GetID(chnStr)) as SocketTextChannel;
-            await channel.SendMessageAsync(broadcastMessage);
-        }
-        [Command("Ban me")]
-        //for dumb people
-        public async Task BanSelf() //why am i doing this lmao
-        {
-            var dumbPerson = Context.Guild.GetUsersAsync().Result.ToList().FirstOrDefault(x => x.Id == Context.User.Id);
-            await dumbPerson.BanAsync();
-            await Context.Channel.SendMessageAsync($"User: **{dumbPerson.Username}#{dumbPerson.Discriminator} wanted to be banned. The ever-merciful bot granted it.");
+            if (channel.Guild == Context.Guild)
+            {
+                await channel.SendMessageAsync(broadcastMessage);
+            }
+            else
+            {
+                await Context.Channel.SendMessageAsync("You may not use this bot to send messages in other servers");
+            }
         }
         [Command("Choose")]
         public async Task Choose(string opt1, string opt2)
         {
+            //possibly broken
             Random rnd = new Random();
             RandomNumberGenerator cryptoRnd = RandomNumberGenerator.Create();
             int output = rnd.Next(1, 3);
-            if(output < 2.5)
+            if (output < 2.5)
             {
                 await Context.Channel.SendMessageAsync("I Choose: " + opt1);
-            } else
+            }
+            else
             {
                 await Context.Channel.SendMessageAsync("I Choose: " + opt2);
             }
         }
-        [Command("GetKerbBackgound")]
-        public async Task GetMyBackground()
+    }
+	//music maybe?
+    [Group("Music")]
+    public class MusicCmd : ModuleBase
+    {
+        [Command("JoinVC")]
+        public async Task JoinChannel(IVoiceChannel channel = null)
         {
-            await Context.Channel.SendFileAsync(@"C:\Users\Kerbinorbiter\Documents\Cursed#\background.jpg", "Here is Kerb's Background");
+            channel = channel ?? (Context.User as IGuildUser)?.VoiceChannel;
+            if (channel == null) { await Context.Channel.SendMessageAsync("User must be in a voice channel, or a voice channel must be passed as an argument."); return; }
+            var audioClient = await channel.ConnectAsync();
         }
-        [Command("GetFile")]
-        public async Task GetFile(string fileLocation)
+        [Command("DisconnectVC")]
+        public async Task LeaveChannel(IVoiceChannel channel = null)
         {
-            if(Context.User.Id == 331159190288596993)
-            {
-                //allow because its me and i should only access my files
-                try {
-                    await Context.Channel.SendFileAsync($@"{fileLocation}", "Here is your file Kerb");
-                } catch
-                {
-                    await Context.Channel.SendMessageAsync("Cannot find this...");
-                }
-                
-            } else
-            {
-                await Context.Channel.SendMessageAsync("You may not access this command"); //don't want others snooping in my stuffs
-            }
-        }
-        [Command("getKOS")]
-        public async Task getKOSScript(string scriptName)
-        {
-            try { 
-                await Context.Channel.SendFileAsync($@"D:\kOS\public{scriptName}", "Here is your kOS file");
-            } catch
-            {
-                await Context.Channel.SendMessageAsync("ERROR"); //checks to see if we can find the file then errors if we cant
-            }
 
         }
     }
